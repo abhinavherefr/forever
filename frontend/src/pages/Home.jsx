fb0707e8f3e61349b4c9693a6447fbdf7e8d3f48
Abhinav Sharma <abhinavsharma@Abhinavs-MacBook-Air.local>
2026-03-03 17:54:01 +0530

    Fix case-sensitive import for BestSeller component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
-import Bestseller from '../components/Bestseller'
+import BestSeller from '../components/BestSeller';
 import OurPolicy from '../components/OurPolicy'
 import NewsletterBox from '../components/NewsletterBox'
 
@@ -11,7 +11,7 @@ const Home = () => {
     <div className=''>
       <Hero />
       <LatestCollection />
-      <Bestseller />
+      <BestSeller />
       <OurPolicy />
       <NewsletterBox />
     </div>
